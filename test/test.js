663de7a8692891373a248afe295420c58e70f8c7
Casey Foster <c@sey.me>
2015-02-05 21:11:28 -0600

    Use more thorough test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var config = require('cogs/src/config');
+var crypto = require('crypto');
 var expect = require('chai').expect;
 var fs = require('fs');
 var getBuild = require('cogs/src/get-build');
@@ -8,9 +9,29 @@ var beforeEach = global.beforeEach;
 var describe = global.describe;
 var it = global.it;
 
+var getHash = function (buffer) {
+  var hash = crypto.createHash('md5');
+  hash.end(buffer);
+  return hash.read().toString('hex');
+};
+
+var getFileHash = function (filePath) {
+  return getHash(fs.readFileSync(filePath));
+};
+
 var FIXTURES = {
   'test/config.json': {
-    'test/input.txt': 'test/output.txt',
+    'test/input.txt': {
+      path: 'test/input.txt',
+      buffer: fs.readFileSync('test/output.txt'),
+      hash: getFileHash('test/output.txt'),
+      requires: [{
+        path: 'test/input.txt',
+        hash: getFileHash('test/input.txt')
+      }],
+      links: [],
+      globs: []
+    },
     'test/error.txt': Error
   }
 };
@@ -24,19 +45,15 @@ Object.keys(FIXTURES).forEach(function (configPath) {
     });
 
     Object.keys(builds).forEach(function (inputPath) {
-      var outputPath = builds[inputPath];
+      var expected = builds[inputPath];
 
       describe(inputPath, function () {
-        var expectsError = outputPath === Error;
+        var expectsError = expected === Error;
 
         it(expectsError ? 'fails' : 'succeeds', function (done) {
           getBuild(inputPath, function (er, build) {
             if (expectsError) expect(er).to.be.an.instanceOf(Error);
-            else {
-              if (er) return done(er);
-              var output = fs.readFileSync(outputPath);
-              expect(build.buffer).to.deep.equal(output);
-            }
+            else expect(build).to.deep.equal(expected);
             done();
           });
         });
